ee3e5c31c27347d1e66ec693b6f8789ffe4c3077
James <j1459.j1459@gmail.com>
2015-11-17 19:34:48 +0800

    more work identified a bunch of packets

--- bin_putty_log.py
+++ bin_putty_log.py
@@ -13,7 +13,7 @@ import os
 
 
 
-def pythonify_packet(packet_line_string):
+def convert_to_expression(packet_line_string):
     """
     Take line from usb-mitm stdout and make it into a python libusb statement
     01[32]: 55 14 c6 16 01 1e 01 ff 00 18 01 1e 01 ff 00 18 01 1e 01 ff 00 18 ea 00 00 00 00 00 00 00 00 00
@@ -28,11 +28,24 @@ def pythonify_packet(packet_line_string):
     formatted_packet_bytes = formatted_packet_bytes[:-2]
     formatted_packet_bytes += "]"
     statement = "dev.write("+endopint+", "+formatted_packet_bytes+")"
-    print statement
+    #print statement
     return statement
 
 
-
+def convert_to_byte_list(packet_line_string):
+    """
+    Take line from usb-mitm stdout and make it into a python libusb statement
+    01[32]: 55 14 c6 16 01 1e 01 ff 00 18 01 1e 01 ff 00 18 01 1e 01 ff 00 18 ea 00 00 00 00 00 00 00 00 00
+    becomes
+    dev.write(1, [0x55, 0x06, 0xc0, 0x02, 0x00, 0xff, 0xff, 0xff, 0x1a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])# Switch to blue
+    """
+    packet_byte_strings = packet_line_string.split(" ")[1:]
+    formatted_packet_bytes = "["
+    for packet_byte_string in packet_byte_strings:
+        formatted_packet_bytes += "0x"+packet_byte_string+", "
+    formatted_packet_bytes = formatted_packet_bytes[:-2]
+    formatted_packet_bytes += "]"
+    return formatted_packet_bytes
 
 
 input_file_path = os.path.join("logs", "2015-11-16_play_game_fo_a_while_putty.log")
@@ -48,19 +61,24 @@ with open(input_file_path, "r") as input_file:
         #81[32]: 55 01 15 6b 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
         endpoint_number = line.split("[")[0]
         byte_strings = line.split(" ")[1:]
-        command = byte_strings[1]
+        command = "0x"+byte_strings[1]+"_0x"+byte_strings[2]
         # Save raw
         output_file_name = endpoint_number+"_"+command+".txt"
         output_file_path = os.path.join(output_dir, output_file_name)
         with open(output_file_path, "a") as output_file:
             output_file.write(line)
         # Save as expression
-        expression = pythonify_packet(line[:-1])
+        expression = convert_to_expression(line[:-1])
         expression_output_file_name = endpoint_number+"_"+command+".py"
         expression_output_file_path = os.path.join(output_dir, expression_output_file_name)
         with open(expression_output_file_path, "a") as expression_output_file:
             expression_output_file.write(expression+"\n")
-
+        # Save as byte list
+        bytelist = convert_to_byte_list(line[:-1])
+        bytelist_output_file_name = endpoint_number+"_"+command+".bytelists"
+        bytelist_output_file_path = os.path.join(output_dir, bytelist_output_file_name)
+        with open(bytelist_output_file_path, "a") as bytelist_output_file:
+            bytelist_output_file.write(bytelist+",\n")
 
 
 
