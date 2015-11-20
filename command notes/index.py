0413e499edd0800c5e7dbca996f3bb5d7bb210e3
James <j1459.j1459@gmail.com>
2015-11-20 19:51:41 +0800

    I think I've got the LED commands documented.

--- command notes/index.py
+++ command notes/index.py
@@ -4,22 +4,26 @@
 
 
 # All known commands for endpoint 01:
-# EP byte1 byte2
-# 01 0x04 0xd2 - TODO
+# Packets to this endpoint seem to always start with 0x55
+# EP byte1 byte2 - description
+# 01 0x04 0xd2 - Unknown, no changes to pads
 # 01 0x06 0xc0 - Immediately switch pad(s) to a single value
 # 01 0x08 0xc2 - Immediately change the colour of one or all pad(s), fade and flash available
 # 01 0x09 0xc3 - set 1 or all pad(s) to a colour with variable flash rates
-# 01 0x0a 0xb1 - TODO
-# 01 0x0a 0xb3 - TODO
-# 01 0x0a 0xd4 - TODO
-# 01 0x0e 0xc8 - Immediately switch to set of colours?
+# 01 0x0a 0xb1 - Unknown, no changes to pads
+# 01 0x0a 0xb3 - Unknown, no changes to pads
+# 01 0x0a 0xd4 - Unknown, no changes to pads
+# 01 0x0e 0xc8 - Immediately switch pad(s) to set of colours
 # 01 0x0f 0xb0 - Startup?
 # 01 0x14 0xc6 - Fade pad(s) to value(s)
 # 01 0x17 0xc7 - Flash all 3 pads with individual colours and rates, either change to new or return to old based on pulse count
 
 
+
+
 # All known commands for endpoint 81:
 # Unconfirmed that these are actually commands.
+# packets containing NFC data seem to begin with 0x56
 # EP byte1 byte2
 # 81 0x01 0x00
 # 81 0x01 0x01
